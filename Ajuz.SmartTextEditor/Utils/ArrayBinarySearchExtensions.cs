e189fe5d66d0fa9ffc3955a5b248209306ca4bcd
ajuz <a.yuzhanin@bk.ru>
2015-10-31 20:37:17 +0500

    Added classes ArrayBinarySearchExtensions, ArraySortExtensions

---
+++ Ajuz.SmartTextEditor/Utils/ArrayBinarySearchExtensions.cs
@@ -0,0 +1,379 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajuz.SmartTextEditor.Utils
+{
+    /// <summary>
+    /// Бинарный поиск в массиве
+    /// </summary>
+    public static class ArrayBinarySearchExtensions
+    {
+        /// <summary>
+        /// Результат бинарного поиска в случае отсуствия совпадений.
+        /// </summary>
+        private const int BinarySearchNotFoundResult = -1;
+
+        /// <summary>
+        /// Выполнить бинарный поиск в сортированном массиве с нулевым начальным индексом 
+        /// всех элементов, соответствующих значению <paramref name="value"/> с 
+        /// использованием сравнения <paramref name="comparison"/>.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов массива</typeparam>
+        /// <param name="sortedArray">Исходный сортированный массив</param>
+        /// <param name="value">Значение для поиска</param>
+        /// <param name="comparison">Правило сравнения элементов</param>
+        /// <returns>Найденные элементы</returns>
+        /// <exception cref="ArgumentNullException">sortedArray, value или comparison содержит null.</exception>
+        /// <seealso cref="https://en.wikipedia.org/wiki/Binary_search_algorithm"/>
+        public static T[] BinarySearchAll<T>(this T[] sortedArray, T value, Comparison<T> comparison)
+        {
+            if (sortedArray == null)
+            {
+                throw new ArgumentNullException("sortedArray");
+            }
+            
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            if (comparison == null)
+            {
+                throw new ArgumentNullException("comparison");
+            }
+
+            var firstFoundIndex = BinarySearchFirst(sortedArray, value, comparison);
+
+            if (firstFoundIndex != BinarySearchNotFoundResult)
+            {
+                var lastFoundIndex = BinarySearchLast(
+                    sortedArray,
+                    firstFoundIndex,
+                    sortedArray.Length - firstFoundIndex,
+                    value,
+                    comparison);
+
+                var searchResult = new T[lastFoundIndex - firstFoundIndex + 1];
+
+                Array.Copy(
+                    sortedArray, 
+                    firstFoundIndex, 
+                    searchResult, 
+                    0, 
+                    searchResult.Length);
+
+                return searchResult;
+            }
+
+            return new T[0];
+        }
+
+        /// <summary>
+        /// Выполнить бинарный поиск в сортированном массиве с нулевым начальным индексом 
+        /// первого элемента, соответствующего значению <paramref name="value"/> с 
+        /// использованием сравнения <paramref name="comparison"/>.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов массива</typeparam>
+        /// <param name="sortedArray">Исходный сортированный массив</param>
+        /// <param name="value">Значение для поиска</param>
+        /// <param name="comparison">Правило сравнения элементов</param>
+        /// <returns>
+        /// Индекс первого (по порядку) элемента, соответствующего условиям поиска.
+        /// Если такой элемент не найден - возвращает -1.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">sortedArray, value или comparison содержит null.</exception>
+        /// <seealso cref="https://en.wikipedia.org/wiki/Binary_search_algorithm"/> 
+        public static int BinarySearchFirst<T>(this T[] sortedArray, T value, Comparison<T> comparison)
+        {
+            if (sortedArray == null)
+            {
+                throw new ArgumentNullException("sortedArray");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            if (comparison == null)
+            {
+                throw new ArgumentNullException("comparison");
+            }
+
+            return BinarySearchFirst<T>(
+                sortedArray, 
+                0, 
+                sortedArray.Length, 
+                value, 
+                comparison);
+        }
+
+        /// <summary>
+        /// Выполнить бинарный поиск в диапазоне элементов сортированного массива 
+        /// (с нулевым начальным индексом) - первого элемента, соответствующего значению 
+        /// <paramref name="value"/> с использованием сравнения <paramref name="comparison"/>.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов массива</typeparam>
+        /// <param name="sortedArray">Исходный сортированный массив</param>
+        /// <param name="index">Индекс начала диапазона поиска.</param>
+        /// <param name="length">Количество элементов диапазона поиска</param>
+        /// <param name="value">Значение для поиска</param>
+        /// <param name="comparison">Правило сравнения элементов</param>
+        /// <returns>
+        /// Индекс первого (по порядку) элемента, соответствующего условиям поиска.
+        /// Если такой элемент не найден - возвращает -1.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// sortedArray, value или comparison содержит null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// index или length меньше 0.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Аргументы index, length приводят к выходу за пределы массива.
+        /// </exception>
+        /// <seealso cref="https://en.wikipedia.org/wiki/Binary_search_algorithm"/>
+        public static int BinarySearchFirst<T>(this T[] sortedArray, int index, int length, T value, Comparison<T> comparison)
+        {
+            if (sortedArray == null)
+            {
+                throw new ArgumentNullException("sortedArray");
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "index",
+                    index,
+                    "Аргумент должен быть не меньше 0.");
+            }
+
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "length",
+                    index,
+                    "Аргумент должен быть не меньше 0.");
+            }
+
+            if (sortedArray.Length < index + length)
+            {
+                throw new ArgumentException(
+                    "Аргументы index, length приводят к выходу за пределы массива.");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            if (comparison == null)
+            {
+                throw new ArgumentNullException("comparison");
+            }
+
+            var firstIndex = index;
+            var lastIndex = index + length - 1;
+
+            while (firstIndex <= lastIndex)
+            {
+                var middleIndex = firstIndex + (lastIndex - firstIndex) / 2;
+
+                if (comparison(sortedArray[middleIndex], value) == 0)
+                {
+                    if (middleIndex == index)
+                    {
+                        // рассматриваемый индекс минимальный,
+                        // значит мы нашли первый элемент с соответствием
+                        return index;
+                    }
+
+                    if (comparison(sortedArray[middleIndex - 1], value) != 0)
+                    {
+                        // предыдущий элемент элемент не удовлетворяет условию сравнения,
+                        // значит мы нашли первый элемент с соответствием 
+                        return middleIndex;
+                    }
+
+                    // элемент найден, но он не первый (из соответствующих)
+                    // продолжаем поиск в "левой" половине
+                    lastIndex = middleIndex - 1;
+                }
+                else
+                {
+                    if (comparison(sortedArray[middleIndex], value) > 0)
+                    {
+                        lastIndex = middleIndex - 1;
+                    }
+                    else
+                    {
+                        firstIndex = middleIndex + 1;
+                    }
+                }
+            }
+
+            return BinarySearchNotFoundResult;
+        }
+
+        /// <summary>
+        /// Выполнить бинарный поиск в диапазоне элементов сортированного массива 
+        /// (с нулевым начальным индексом) - последнего элемента, соответствующего значению 
+        /// <paramref name="value"/> с использованием сравнения <paramref name="comparison"/>.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов массива</typeparam>
+        /// <param name="sortedArray">Исходный сортированный массив</param>
+        /// <param name="index">Индекс начала диапазона поиска.</param>
+        /// <param name="length">Количество элементов диапазона поиска</param>
+        /// <param name="value">Значение для поиска</param>
+        /// <param name="comparison">Правило сравнения элементов</param>
+        /// <returns>
+        /// Индекс последнего (по порядку) элемента, соответствующего условиям поиска.
+        /// Если такой элемент не найден - возвращает -1.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// sortedArray, value или comparison содержит null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// index или length меньше 0.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Аргументы index, length приводят к выходу за пределы массива.
+        /// </exception>
+        /// <seealso cref="https://en.wikipedia.org/wiki/Binary_search_algorithm"/>
+        public static int BinarySearchLast<T>(this T[] sortedArray, T value, Comparison<T> comparison)
+        {
+            if (sortedArray == null)
+            {
+                throw new ArgumentNullException("sortedArray");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            if (comparison == null)
+            {
+                throw new ArgumentNullException("comparison");
+            }
+
+            return BinarySearchLast<T>(
+                sortedArray,
+                0,
+                sortedArray.Length,
+                value,
+                comparison);
+        }
+
+        /// <summary>
+        /// Выполнить бинарный поиск в диапазоне элементов сортированного массива 
+        /// (с нулевым начальным индексом) - последнего элемента, соответствующего значению 
+        /// <paramref name="value"/> с использованием сравнения <paramref name="comparison"/>.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов массива</typeparam>
+        /// <param name="sortedArray">Исходный сортированный массив</param>
+        /// <param name="index">Индекс начала диапазона поиска.</param>
+        /// <param name="length">Количество элементов диапазона поиска</param>
+        /// <param name="value">Значение для поиска</param>
+        /// <param name="comparison">Правило сравнения элементов</param>
+        /// <returns>
+        /// Индекс последнего (по порядку) элемента, соответствующего условиям поиска.
+        /// Если такой элемент не найден - возвращает -1.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// sortedArray, value или comparison содержит null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// index или length меньше 0.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Аргументы index, length приводят к выходу за пределы массива.
+        /// </exception>
+        /// <seealso cref="https://en.wikipedia.org/wiki/Binary_search_algorithm"/>
+        public static int BinarySearchLast<T>(this T[] sortedArray, int index, int length, T value, Comparison<T> comparison)
+        {
+            if (sortedArray == null)
+            {
+                throw new ArgumentNullException("sortedArray");
+            }
+
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "index",
+                    index,
+                    "Аргумент должен быть не меньше 0.");
+            }
+
+            if (length < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "length",
+                    index,
+                    "Аргумент должен быть не меньше 0.");
+            }
+
+            if (sortedArray.Length < index + length)
+            {
+                throw new ArgumentException(
+                    "Аргументы index, length приводят к выходу за пределы массива.");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            if (comparison == null)
+            {
+                throw new ArgumentNullException("comparison");
+            }
+
+            var maxIndex = index + length - 1;
+            var firstIndex = index;
+            var lastIndex = maxIndex;
+
+            while (firstIndex <= lastIndex)
+            {
+                var middleIndex = firstIndex + (lastIndex - firstIndex) / 2;
+
+                if (comparison(sortedArray[middleIndex], value) == 0)
+                {
+                    if (middleIndex == maxIndex)
+                    {
+                        // рассматриваемый индекс максимальный,
+                        // значит мы нашли последний элемент с соответствием
+                        return maxIndex;
+                    }
+
+                    if (comparison(sortedArray[middleIndex + 1], value) != 0)
+                    {
+                        // следующий элемент элемент не удовлетворяет условию сравнения,
+                        // значит мы нашли последний элемент с соответствием 
+                        return middleIndex;
+                    }
+
+                    // элемент найден, но он не последний (из соответствующих)
+                    // продолжаем поиск в "правой" половине
+                    firstIndex = middleIndex + 1;
+                }
+                else
+                {
+                    if (comparison(sortedArray[middleIndex], value) > 0)
+                    {
+                        lastIndex = middleIndex - 1;
+                    }
+                    else
+                    {
+                        firstIndex = middleIndex + 1;
+                    }
+                }
+            }
+
+            return BinarySearchNotFoundResult;
+        }
+    }
+}
---
+++ Ajuz.SmartTextEditor/Utils/ArraySortExtensions.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajuz.SmartTextEditor.Utils
+{
+    /// <summary>
+    /// Сортировка массива
+    /// </summary>
+    public static class ArraySortExtensions
+    {
+        /// <summary>
+        /// Получить топовые (наибольшие) элементы массива, упорядоченные по убыванию.
+        /// </summary>
+        /// <typeparam name="T">Тип элементов массива</typeparam>
+        /// <param name="array">Массив</param>
+        /// <param name="comparison">Правило сравнения элементов</param>
+        /// <param name="countOfTopElements">Максимальное количество топовых элементов</param>
+        /// <returns>
+        /// Массив из топовых (наибольших) элементов исходного массива, 
+        /// упорядоченных по убыванию.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        /// array или comparison содержит null.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// countOfTopElements содержит значение меньше 0.
+        /// </exception>
+        public static T[] GetTopOrderedByDescending<T>(this T[] array, Comparison<T> comparison, int countOfTopElements) where T: DictionaryWord
+        {            
+            if (array == null)
+            {
+                throw new ArgumentNullException("array");
+            }
+
+            if (comparison == null)
+            {
+                throw new ArgumentNullException("comparison");
+            }
+
+            if (countOfTopElements < 0)
+            {
+                throw new ArgumentOutOfRangeException(
+                    "countOfTopElements",
+                    countOfTopElements,
+                    "Аргумент должен быть не меньше 0.");
+            }
+
+            if (countOfTopElements == 0)
+            {
+                return new T[0];
+            }
+
+            // Алгоритм:
+            // В цикле пробегаемся по всем элементам исходного массива array
+            // и заполняем (в порядке убывания) максимальными из них массив topSortedBuffer.
+            // Массив topSortedBuffer имеет фиксированный размер = countOfTopElements.
+            // Если массив topSortedBuffer полный - то очередной добавляемый в него элемент
+            // вытеснит элемент с максимальным индексом (т.е. наименьший из ранее добавленных).
+            // Фактическое количество хранящихся в нем элементов содержится в topSortedCount.
+            
+            // В качестве topSortedBuffer удобнее было бы использовать LinkedList<T>, 
+            // но значительно падает производительность.
+
+            var topSortedBuffer = new T[countOfTopElements];
+            var topSortedCount = 0;
+            var addedToTopSortedBuffer = false;
+
+            for (int inputIndex = 0; inputIndex < array.Length; inputIndex++)
+            {
+                addedToTopSortedBuffer = false;
+
+                for (int topSortedIndex = 0; topSortedIndex < topSortedCount; topSortedIndex++)
+                {
+                    if (comparison(topSortedBuffer[topSortedIndex], array[inputIndex]) < 0)
+                    {
+                        Array.Copy(
+                            topSortedBuffer,
+                            topSortedIndex,
+                            topSortedBuffer,
+                            topSortedIndex + 1,
+                            countOfTopElements - topSortedIndex - 1);
+
+                        topSortedBuffer[topSortedIndex] = array[inputIndex];
+
+                        if (topSortedCount < topSortedBuffer.Length)
+                        {
+                            topSortedCount++;
+                        }
+                        
+                        addedToTopSortedBuffer = true;
+                        break;
+                    }
+                }
+
+                if (topSortedCount < topSortedBuffer.Length && !addedToTopSortedBuffer)
+                {
+                    topSortedBuffer[topSortedCount] = array[inputIndex];
+                    topSortedCount++;
+                }
+            }
+           
+            return topSortedBuffer.Take(topSortedCount).ToArray();
+        }
+    }
+}
