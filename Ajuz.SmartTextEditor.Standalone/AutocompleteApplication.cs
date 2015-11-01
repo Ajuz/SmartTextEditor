9e2fab5c770f2e9aee017b4bc17528356c650d8e
ajuz <a.yuzhanin@bk.ru>
2015-11-01 12:31:54 +0500

    Added assembly for Standalone version of SmartTextEditor. Contains class for autocomplete words: AutocompleteApplication

---
+++ Ajuz.SmartTextEditor.Standalone/AutocompleteApplication.cs
@@ -0,0 +1,92 @@
+﻿using Ajuz.SmartTextEditor.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ajuz.SmartTextEditor.Standalone
+{
+    /// <summary>
+    /// Приложение поиска автодополнений
+    /// </summary>
+    internal class AutocompleteApplication
+    {
+        /// <summary>
+        /// Обеспечивает чтение словарных слов
+        /// </summary>
+        private IInputReader<DictionaryWord> _dictionaryWordsInputReader;
+
+        /// <summary>
+        /// Обеспечивает чтение начальных частей слов
+        /// </summary>
+        private IInputReader<string> _beginningsOfWordsInputReader;
+
+        /// <summary>
+        /// Обеспечивает вывод найденных автодополнений
+        /// </summary>
+        private IOutputWriter<DictionaryWord> _autocompleteOutputWriter;
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса AutocompleteApplication
+        /// </summary>
+        /// <param name="dictionaryWordsInputReader">
+        /// Обеспечивает чтение словарных слов
+        /// </param>
+        /// <param name="beginningsOfWordsInputReader">
+        /// Обеспечивает чтение начальных частей слов
+        /// </param>
+        /// <param name="autocompleteOutputWriter">
+        /// Обеспечивает вывод найденных автодополнений
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// dictionaryWordsInputReader, beginningsOfWordsInputReader или
+        /// autocompleteOutputWriter содержит null.
+        /// </exception>
+        public AutocompleteApplication(
+            IInputReader<DictionaryWord> dictionaryWordsInputReader,
+            IInputReader<string> beginningsOfWordsInputReader,
+            IOutputWriter<DictionaryWord> autocompleteOutputWriter)
+        {
+            if (dictionaryWordsInputReader == null)
+            {
+                throw new ArgumentNullException("dictionaryWordsInputReader");
+            }
+
+            if (beginningsOfWordsInputReader == null)
+            {
+                throw new ArgumentNullException("beginningsOfWordsInputReader");
+            }
+
+            if (autocompleteOutputWriter == null)
+            {
+                throw new ArgumentNullException("autocompleteOutputWriter");
+            }
+
+            _dictionaryWordsInputReader = dictionaryWordsInputReader;
+            _beginningsOfWordsInputReader = beginningsOfWordsInputReader;
+            _autocompleteOutputWriter = autocompleteOutputWriter;
+        }
+
+        /// <summary>
+        /// Найти автодополнения
+        /// </summary>
+        public void SearchAutocomplete()
+        {
+            IEnumerable<DictionaryWord> dictionaryWords = 
+                _dictionaryWordsInputReader.Read();
+            
+            var autocompleteSearcher = new FastAutocompleteSearcher(dictionaryWords);
+            
+            IEnumerable<string> beginningsOfWords = _beginningsOfWordsInputReader.Read();
+
+            foreach (var beginningOfWord in beginningsOfWords)
+            {
+                IEnumerable<DictionaryWord> autocompleteCollection =
+                    autocompleteSearcher.FindWordsStartsWith(beginningOfWord);
+
+                _autocompleteOutputWriter.Write(autocompleteCollection);
+            }
+        }
+    }
+}
---
+++ Ajuz.SmartTextEditor.Standalone/Program.cs
@@ -0,0 +1,33 @@
+﻿using Ajuz.SmartTextEditor.IO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ajuz.SmartTextEditor.Standalone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputStreamReader = new StreamReader(
+                Console.OpenStandardInput(),
+                Console.InputEncoding);
+
+            var outputStreamWriter = new StreamWriter(
+                Console.OpenStandardOutput(),
+                Console.OutputEncoding);
+
+            var autocompleteAppication = new AutocompleteApplication(
+                new DictionaryWordsInputReader(inputStreamReader),
+                new BeginningsOfWordsInputReader(inputStreamReader),
+                new AutocompleteOutputWriter(outputStreamWriter));
+
+            autocompleteAppication.SearchAutocomplete();
+        }
+    }
+}
